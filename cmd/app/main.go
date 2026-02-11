0a4f77ede1a88f09bb3dc1edb4d25448ac7ab8bc
aristelcrowley <andhikapratamaunsri@gmail.com>
2026-02-11 22:19:01 +0700

    feat(main): enhance server initialization with share and upload directory options

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,13 +4,18 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"qr-cross-file-transfer/internal/ip"
 	"qr-cross-file-transfer/internal/server"
+	"qr-cross-file-transfer/internal/utils"
 )
 
 func main() {
+	port := flag.Int("port", 8080, "port to listen on")
 	iface := flag.String("interface", "", "override LAN IP address")
+	shareDir := flag.String("share", ".", "directory to share for download")
+	uploadDir := flag.String("uploads", "./uploads", "directory to save uploaded files")
 	flag.Parse()
 
 	lanIP := *iface
@@ -22,16 +27,24 @@ func main() {
 		lanIP = detected
 	}
 
-	
-	port := flag.Int("port", 8080, "port to listen on")
+	if err := os.MkdirAll(*uploadDir, 0o755); err != nil {
+		log.Fatalf("Cannot create upload directory: %v", err)
+	}
+
 	addr := fmt.Sprintf(":%d", *port)
 	url := fmt.Sprintf("http://%s:%d", lanIP, *port)
 
-
+	fmt.Println("  qr-cross-file-transfer")
 	fmt.Printf("  Local:   http://localhost:%d\n", *port)
 	fmt.Printf("  Network: %s\n", url)
+	fmt.Printf("  Share:   %s\n", *shareDir)
+	fmt.Printf("  Uploads: %s\n", *uploadDir)
 
-	app := server.New()
+	if err := utils.GenerateQR(url); err != nil {
+		log.Printf("Warning: could not render QR code: %v", err)
+	}
+
+	app := server.New(*shareDir, *uploadDir)
 
 	if err := app.Listen(addr); err != nil {
 		log.Fatalf("Server failed: %v", err)
