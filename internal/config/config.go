095af118d75dbd4632955a00b77cec6e6e763fdb
aristelcrowley <andhikapratamaunsri@gmail.com>
2026-02-11 22:54:28 +0700

    style(config): format AppConfig struct for improved readability

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,10 @@
 package config
 
 type AppConfig struct {
-	Port int
-	Interface string
-	LanIP string
-	ShareDir string
-	UploadDir string
+	Port       int
+	Interface  string
+	LanIP      string
+	ShareDir   string
+	UploadDir  string
 	NetworkURL string
 }
