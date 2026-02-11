a09d6a20c0655228fca62d02ceb131fd8956192e
aristelcrowley <andhikapratamaunsri@gmail.com>
2026-02-11 22:16:41 +0700

    feat(server): update server initialization to accept share and upload directories

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,16 +2,27 @@ package server
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 
+	"qr-cross-file-transfer/internal/controllers"
 	"qr-cross-file-transfer/internal/routes"
 )
 
-func New() *fiber.App {
+func New(shareDir, uploadDir string) *fiber.App {
 	app := fiber.New(fiber.Config{
 		AppName: "qr-cross-file-transfer",
+		BodyLimit: 2 * 1024 * 1024 * 1024,
 	})
 
-	routes.RegisterRoutes(app)
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "GET,POST,OPTIONS",
+	}))
+
+	fc := controllers.NewFileController(shareDir)
+	uc := controllers.NewUploadController(uploadDir)
+
+	routes.RegisterRoutes(app, fc, uc)
 
 	return app
 }
