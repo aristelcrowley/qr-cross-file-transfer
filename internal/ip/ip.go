50dee154ee35f345342006b881bc1d29d09f2609
aristelcrowley <andhikapratamaunsri@gmail.com>
2026-02-11 22:01:45 +0700

    fix: change the prioritizing in ip detection as previously often selected virtual interfaces

--- internal/ip/ip.go
+++ internal/ip/ip.go
@@ -1,28 +1,45 @@
 package ip
 
 import (
-	"fmt"
-	"net"
+    "fmt"
+    "net"
+    "strings"
 )
 
 func GetLocalIP() (string, error) {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		return "", fmt.Errorf("failed to get interface addresses: %w", err)
-	}
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        return "", err
+    }
 
-	for _, addr := range addrs {
-		ipNet, ok := addr.(*net.IPNet)
-		if !ok {
-			continue
-		}
+    var candidates []string
 
-		if ipNet.IP.IsLoopback() || ipNet.IP.To4() == nil {
-			continue
-		}
+    for _, addr := range addrs {
+        ipNet, ok := addr.(*net.IPNet)
+        if !ok {
+            continue
+        }
 
-		return ipNet.IP.String(), nil
-	}
+        if !ipNet.IP.IsLoopback() && ipNet.IP.To4() != nil {
+            ip := ipNet.IP.String()
 
-	return "", fmt.Errorf("no suitable LAN IP address found")
+            if strings.HasPrefix(ip, "10.") {
+                return ip, nil 
+            }
+
+            if strings.HasPrefix(ip, "192.168.") {
+                if !strings.HasSuffix(ip, ".1") {
+                    candidates = append(candidates, ip)
+                }
+            }
+
+            candidates = append(candidates, ip)
+        }
+    }
+
+    if len(candidates) > 0 {
+        return candidates[0], nil
+    }
+
+    return "", fmt.Errorf("no suitable LAN IP address found")
 }
